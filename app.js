d160b5c70ecbeb312fcc1697631fc628965feb74
Artem <maltsev.qw@gmail.com>
2019-04-24 12:02:37 +0300

    add in index.html slider

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//add-to-cart
 var productsCountEl = document.getElementById('products-count');
 console.log(productsCountEl);
 
@@ -11,15 +12,63 @@ productsCountEl.textContent = prevProductsCount +1
 })
 }
 
-var heartLike = document.getElementsByClassName('like');
-console.log(heartLike);
-var addLike = document.querySelectorAll(".heart");
-console.log(addLike);
-/*
-for(var i = 0; i < heartLike.length; i++){
-	heartLike[i].addEventListener("click", function(){
-var prevheartLike = +heartLike.textContent
-heartLike.textContent = prevheartLike+1
+
+//change like button state
+
+var likeButtons = document.querySelectorAll('.heart');
+
+for (var i = 0; i < likeButtons.length; i++) {
+	likeButtons[i].addEventListener('click', function(){
+	   if(this.classList.contains('liked')){
+		this.classList.remove('liked')
+	} 	else{
+		this.classList.add("liked")
+	}
 })
 }
+
+//change product quantity
+var decrementButton = document.querySelectorAll('.decrement-button')[0];
+var incrementButton = document.querySelectorAll('.increment-button')[0];
+var productQuantity = document.querySelectorAll('.quantity-input input')[0];
+
+/*function toggleButtonState(count){
+if(count <= 1 ){
+	decrementButton.disabled = true;
+}else if(count >= 5 ){
+	incrementButton.disabled = true;
+} else {
+	incrementButton.disabled = false;
+	decrementButton.disabled = false;
+  }
+}
 */
+var minProductCount = 1;
+var maxProductCount = 5;
+function toggleButtonState(count){
+	decrementButton.disabled = count <= minProductCount;
+	incrementButton.disabled = count >= maxProductCount;
+}
+
+var currentCount = +productQuantity.value
+toggleButtonState(currentCount)
+
+incrementButton.addEventListener('click', function(){
+var currentCount = +productQuantity.value
+var nextCount = currentCount + 1
+productQuantity.value = nextCount
+toggleButtonState(nextCount)
+});
+
+decrementButton.addEventListener('click', function(){
+var currentCount = +productQuantity.value
+var nextCount = currentCount - 1
+productQuantity.value = nextCount
+toggleButtonState(nextCount)
+});
+
+$('.slider-block').slick({
+    dots:true,
+    slidesToShow: 1,
+  });
+
